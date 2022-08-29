a5d9ffdc5e70d07b9dd8fdeacfdc414b15708a2c
VitorPaulon <vitorpaulon@gmail.com>
2022-08-29 20:54:56 -0300

    Atualização

---
+++ script.js
@@ -0,0 +1,8 @@
+function desligar(lampadaDesligada, fundoDesligada){
+  document.getElementById('lampada').style.backgroundColor = lampadaDesligada
+  document.getElementById('fundo').style.backgroundColor = fundoDesligada
+}
+function ligar(lampadaLigada, fundoLigada){
+  document.getElementById('lampada').style.backgroundColor = lampadaLigada
+  document.getElementById('fundo').style.backgroundColor = fundoLigada
+}
